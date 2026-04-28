5e7a8f101ba2d5082904ef98156b0394547ee298
srikotala <133939190+itzskoala@users.noreply.github.com>
2026-04-28 10:01:41 -0500

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+       let map = {};
+        for(let i =0; i < nums.length; i++){
+            if(map[nums[i]] === undefined){ //if there is a spot empty then add it
+                map[nums[i]] = i;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+//hashtable, HT: 1: True 2: 
